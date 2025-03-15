e1f31a0130d7e3ec43841e5dcc132ad9fef298f8
CHIA-YI <chia-yi@CHIA-YIdeMacBook-Pro.local>
2025-03-16 02:01:26 +0800

    feat: homework

--- week-1-clear.py
+++ week-1-clear.py
@@ -39,6 +39,28 @@ for filename in tqdm(os.listdir(directory)):
 
 if prefix_data:
   for prefix,data in prefix_data.items():
+    folder = './cleaned'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     output_filename = f"{prefix}_combined.csv"
-    data.to_csv(output_filename, index=False, encoding="utf-8", quoting=csv.QUOTE_NONE, escapechar='\\')
+    file_path = os.path.join(folder, output_filename)
+    # data.to_csv(file_path, index=False, encoding="utf-8", quoting=csv.QUOTE_NONE, escapechar='\\')
+    data.to_csv(file_path, index=False, encoding="utf-8")
     print(f"已保存 {output_filename}")
+
+
+input_folder = "./cleaned"
+output_file = "./all-boards.csv"
+
+all_data = pd.DataFrame()
+
+for filename in tqdm(os.listdir(input_folder)):
+    if filename.endswith('.csv'):
+        file_path = os.path.join(input_folder, filename)
+        print(file_path)
+        df = pd.read_csv(file_path, quotechar='"', dtype=str)
+        all_data = pd.concat([all_data, df], ignore_index=True)
+
+all_data.to_csv(output_file, index=False, encoding="utf-8")
+
+print(f"全部版面整合完畢 {output_file}")
